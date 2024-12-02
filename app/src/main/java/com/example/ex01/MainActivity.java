4efb66d74aaf86e6dc24804af56fb179800f7dc6
gs7073 <gs7073@bs.amalnet.k12.il>
2024-12-02 13:20:55 +0200

    First Commit Second Commit Third Commit

--- app/src/main/java/com/example/ex01/MainActivity.java
+++ app/src/main/java/com/example/ex01/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //תביא לי מאה
+        //אלברט המלך
+        //בהצלחה לכולם
 
 
 
